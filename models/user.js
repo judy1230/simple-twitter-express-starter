9a84182f59ef79d2a3380815c12aadc854cc53b8
Henry Han <potatoeater0127@gmail.com>
2019-11-25 17:05:34 +0800

    (config) set up User model's schema

--- models/user.js
+++ models/user.js
@@ -1,7 +1,17 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-  }, {});
+  const User = sequelize.define(
+    'User',
+    {
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      name: DataTypes.STRING,
+      avatar: DataTypes.STRING,
+      introduction: DataTypes.TEXT,
+      role: DataTypes.STRING
+    },
+    {}
+  );
   User.associate = function(models) {
   };
   return User;
