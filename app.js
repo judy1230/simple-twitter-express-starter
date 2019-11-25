68d2d1b30e6a3283a5d8bbe927bc5f529c3bb767
Henry Han <potatoeater0127@gmail.com>
2019-11-25 18:43:54 +0800

    (config) set up session/passport/flash

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const flash = require('connect-flash');
+const session = require('express-session');
+const passport = require('./config/passport');
 const helpers = require('./_helpers');
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }), bodyParser.json());
+app.use(session({ secret: "secret", resave: false, saveUninitialized: false }));
+app.use(passport.initialize(), passport.session());
+app.use(flash(), (req, res) => {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.user = req.user;
+  next();
+});
 
 // use helpers.getUser(req) to replace req.user
 // use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
