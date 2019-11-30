e9ba141faf39465f3f34ee253b3ab7a5bfba1ea9
judy1230 <50729317+judy1230@users.noreply.github.com>
2019-11-30 15:35:04 +0800

    add User.hasMany(models.Reply)

--- models/user.js
+++ models/user.js
@@ -14,6 +14,7 @@ module.exports = (sequelize, DataTypes) => {
   );
   User.associate = function (models) {
     User.hasMany(models.Tweet)
+    User.hasMany(models.Reply)
     User.belongsToMany(models.Tweet, {
       through: models.Like,
       foreignKey: 'UserId',
