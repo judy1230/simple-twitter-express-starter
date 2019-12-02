4ce801b83419f1f425e1fcb7b5ff5f266ac0510c
judy1230 <50729317+judy1230@users.noreply.github.com>
2019-12-02 21:13:00 +0800

    fix following issue

--- controllers/tweetsController.js
+++ controllers/tweetsController.js
@@ -1,41 +1,34 @@
+const bcrypt = require('bcrypt-nodejs')
 const db = require('../models')
 const User = db.User
 const Tweet = db.Tweet
 const Reply = db.Reply
 const Like = db.Like
 const pageLimit = 10
+const Followship = db.Followship
 
 const tweetsController = {
-	getTweets:  (req, res) => {
-			return User.findAll({
-				include: [
-					{ model: User, as: 'Followers' },
-					{ model: User, as: 'Followings' },
-				]
-			}).then(users => {
-				console.log('users', users)
-				users.map(user => ({
-					...user.dataValues,
-					//計算追蹤者人數
-					FollowerCount: user.Followers.length,
-					//判斷目前登入使用者是否已追中該User物件
-					isFollowed: req.user.Followings.map(d => d.id).includes(user.id)
-				}))
-				//依追蹤者人數排列清單
-				return users.sort((a, b) => b.FollowerCount - a.FollowerCount)
-			}).then(users => {
-				res.render('tweets',
-					{
-						TopFollowers: users,
-					})
-			})
+	getTweets: (req, res) => {
+		return User.findAll({
+			include: [
+				{ model: User, as: 'Followers' }
+			]
+		}).then(users => {
+			users = users.map(user => ({
+				...user.dataValues,
+				FollowerCount: user.Followers.length,
+				isFollowed: req.user.Followings.map(d => d.id).includes(user.id)
+			}))
+			users = users.sort((a, b) => b.FollowerCount - a.FollowerCount)
+			return res.render('tweets', { users: users })
+		})
 
 	},
 	getTweetReplies: (req, res) => {
 		return Tweet.findByPk(req.params.id, {
 			include: [
 				//Reply
-				 { model: Reply, include: Tweet },
+				{ model: Reply, include: Tweet },
 				// { model: Tweet, as: 'LikedTweets' },
 				// { model: User, as: 'Followers' },
 				// { model: User, as: 'Followings' }
@@ -48,39 +41,6 @@ const tweetsController = {
 						tweet: tweet
 					})
 			})
-		// 	include: [
-		// 		//Tweet,
-		// 		{ model: Tweet, where: {Id: req.paras.id} },
-		// 		{ model: Reply, include: Tweet },
-		// 		{ model: Tweet, as: 'LikedTweets' },
-		// 		{ model: User, as: 'Followers' },
-		// 		{ model: User, as: 'Followings' }
-		// 	]
-		// })
-		// 	.then(user => {
-		// 		//console.log('user.Tweets', user.LikedTweets.length)
-		// 		res.render('reply',
-		// 			{
-		// 				user: user
-		// 			})
-		// 	})
-		// return User.findByPk(req.params.id, {
-		// 	include: [
-		// 		//Tweet,
-		// 		{ model: Tweet, where: {Id: req.paras.id} },
-		// 		{ model: Reply, include: Tweet },
-		// 		{ model: Tweet, as: 'LikedTweets' },
-		// 		{ model: User, as: 'Followers' },
-		// 		{ model: User, as: 'Followings' }
-		// 	]
-		// })
-		// 	.then(user => {
-		// 		//console.log('user.Tweets', user.LikedTweets.length)
-		// 		res.render('reply',
-		// 			{
-		// 				user: user
-		// 			})
-		// 	})
 	},
 	postTweet: (req, res) => {
 		if (!req.body.text) {
@@ -90,9 +50,9 @@ const tweetsController = {
 		if (!req.body.text.length > 140) {
 			return res.redirect('/tweets')
 
-		}else {
-		  Tweet.create({
-				UserId: "1",
+		} else {
+			Tweet.create({
+				UserId: req.user.id,
 				description: req.body.text,
 			})
 				.then((tweet) => {
