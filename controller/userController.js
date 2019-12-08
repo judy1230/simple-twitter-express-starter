9eb69487ff5d4dc0a9c5a3da341e0d8ddbda9d31
Terrence Yang <encoreyang78@gmail.com>
2019-12-08 14:54:46 +0800

    Add Get&Put editUser and edithandlebars and routes

--- controller/userController.js
+++ controller/userController.js
@@ -9,7 +9,57 @@ const sequelize = require('sequelize')
 
 
 const userController = {
-
+  //編輯資料
+  editUser: (req, res) => {
+    if (Number(req.params.id) !== helpers.getUser(req).id) {
+      req.flash('error_messages', '您無權編輯他人檔案')
+      return res.redirect(`/users/${req.params.id}/tweets`)
+    } else {
+      return User.findByPk(req.params.id).then(user => {
+        return res.render('edit')
+      })
+    }
+  },
+  putUser: (req, res) => {
+    if (Number(req.params.id) !== Number(helpers.getUser(req).id)) {
+      req.flash('error_messages', '您無權編輯他人檔案')
+      return res.redirect(`/users/${req.params.id}/tweets`)
+    }
+    if (!req.body.name) {
+      req.flash('error_messages', "name didn't exist")
+      return res.redirect('back')
+    }
+    const { file } = req
+    if (file) {
+      imgur.setClientID(IMGUR_CLIENT_ID)
+      imgur.upload(file.path, (err, img) => {
+        return User.findByPk(req.params.id).then(user => {
+          user
+            .update({
+              name: req.body.name,
+              introduction: req.body.introduction,
+              avatar: file ? img.data.link : user.avatar
+            })
+            .then(user => {
+              req.flash('success_messages', 'user was successfully to update')
+              res.redirect(`/users/${req.params.id}/tweets`)
+            })
+        })
+      })
+    } else
+      return User.findByPk(req.params.id).then(user => {
+        user
+          .update({
+            name: req.body.name,
+            introduction: req.body.introduction,
+            avatar: user.avatar
+          })
+          .then(user => {
+            req.flash('success_messages', 'user was successfully to update')
+            res.redirect(`/users/${req.params.id}/tweets`)
+          })
+      })
+  },
   //追蹤功能
   addFollowing: (req, res) => {
     return Followship.create({
--- routes/routes.js
+++ routes/routes.js
@@ -5,6 +5,9 @@ const adminController = require('../controller/adminController')
 const userController = require('../controllers/userController.js')
 const passport = require('../config/passport')
 
+const multer = require('multer')
+const upload = multer({ dest: 'temp/' })
+
 const authenticated = (req, res, next) => {
   if (req.isAuthenticated()) {
     return next()
@@ -19,9 +22,14 @@ const authenticatedAdmin = (req, res, next) => {
   res.redirect('/signin')
 }
 
+//編輯使用者
+router.get('./user/:id/edit', authenticated, userController.editUser)
+router.put('/user/:id/edit', authenticated, upload.sigle('avatar'), userController.putUSer)
 //追蹤路由
 router.get('/users/:id/followings', authenticated, userController.getFollowings)
 router.get('/users/:id/followers', authenticated, userController.getFollowers)
 
 router.post('/following/:userId', userController.addFollowing)
 router.delete('/following/:userId', userController.removeFollowing)
+
+module.exports = router
